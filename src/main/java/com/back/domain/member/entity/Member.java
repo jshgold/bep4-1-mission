d4864a938ed05d8d20a8ab737e370f59852192cc
devho <jshgold93@gmail.com>
2026-06-13 13:05:16 +0900

    멤버 엔티티 생성

--- src/main/java/com/back/domain/member/entity/Member.java
+++ src/main/java/com/back/domain/member/entity/Member.java
@@ -1,4 +1,16 @@
 package com.back.domain.member.entity;
 
-public class Member {
+import com.back.global.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Member extends BaseEntity {
+    String userName;
+    String password;
+    String nickName;
 }
